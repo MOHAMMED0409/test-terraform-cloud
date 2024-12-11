2329eed3834c030daaa1ab2b0a168fef889cd7d3
mohammed khashif <mohammedkhashif02@gmail.com>
2024-12-11 16:20:44 +0530

    commit file

---
+++ main.tf
@@ -0,0 +1,35 @@
+## PUT TERRAFORM CLOUD BLOCK HERE!  ##
+
+terraform {
+  cloud {
+
+    organization = "terraform-tut-1"
+
+    workspaces {
+      name = "test-1"
+    }
+  }
+}
+
+# Variable blocks directly within the main.tf. No arguments necessary.
+# variable "aws_access_key" {}
+# variable "aws_secret_key" {}
+variable "region" {}
+
+# provider arguments call on the variables which then call on terraform.tfvars for the values.
+provider "aws" {
+  # access_key = var.aws_access_key
+  # secret_key = var.aws_secret_key
+  region     = var.region
+}
+
+# Add .gitignore file in this directory with the terraform.tfvars
+
+resource "aws_instance" "tc_instance" {
+  ami           = "ami-0e2c8caa4b6378d8c"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "TC-triggered-instance"
+  }
+}
